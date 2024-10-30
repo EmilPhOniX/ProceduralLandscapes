6ceedca72cc6778c89e52bb785ee2ca6214d1fae
Emilien <emilien.derobert@gmail.com>
2024-10-30 14:46:32 +0100

    Déformation du terrain
    Cette version est un test je vais essayer dans le main de prendre la version du prof

--- Assets/CreateTerrain.cs
+++ Assets/CreateTerrain.cs
@@ -24,12 +24,27 @@ public class Terrain : MonoBehaviour
     private bool IsInRotMode = false;
     private bool IsCharacterActive = false;
 
+    //Deformation usage
+    [Range(1.5f, 5f)]
+    public float radius = 2f;
+
+    [Range(0.5f, 5f)]
+    public float deformationStrength = 2f;
+
+    public AnimationCurve attenuationCurve;
+    private Mesh mesh;
+    private Vector3[] vertices, modifiedVerts;
+
 
     // Méthode appelée au démarrage
     void Start()
     {
         // Créer le terrain
         CreerTerrain();
+
+        mesh = GetComponentInChildren<MeshFilter>().mesh;
+        vertices = mesh.vertices;
+        modifiedVerts = mesh.vertices;
     }
 
     // Méthode appelée à chaque frame
@@ -37,6 +52,40 @@ public class Terrain : MonoBehaviour
     {
         HandleTerrainRotation();
         HandleCharacterSpawn();
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            for (int v = 0; v < modifiedVerts.Length; v++)
+            {
+                Vector3 distance = modifiedVerts[v] - hit.point;
+
+                float smoothingFactor = 2f;
+                float force = deformationStrength / (1f + hit.point.sqrMagnitude);
+
+                if (distance.sqrMagnitude < radius)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        modifiedVerts[v] = modifiedVerts[v] + (Vector3.up * force) / smoothingFactor;
+                    }
+                    else if (Input.GetMouseButtonDown(1))
+                    {
+                        modifiedVerts[v] = modifiedVerts[v] + (Vector3.down * force) / smoothingFactor;
+                    }
+                }
+            }
+        }
+        RecalculateMesh();
+    }
+
+    void RecalculateMesh()
+    {
+        mesh.vertices = modifiedVerts;
+        GetComponentInChildren<MeshCollider>().sharedMesh = mesh;
+        mesh.RecalculateNormals();
     }
 
     // Méthode pour créer le terrain
